02f467dc75914ce2a4735c725f67c9ca73ed9ac5
Zerch <sergioibanezmoreno@gmail.com>
2023-03-07 17:28:27 +0100

    Deleted log

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,6 @@
 const logger = require("../logger");
 
 function logErrorMiddleware(err, req, res, next) {
-  console.log("middleware!!!");
   logger.error(err);
   res.status(500).json({ error: "Error fetching data" });
 }
